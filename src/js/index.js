441c1d98ebcb6649e5bf2784bdf85250b6fbec86
javierdelpozolopez <javierdelpozolopez@gmail.com>
2019-01-30 01:32:10 -0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,38 @@
+import {saludo, despedida} from "./modules/example"
+import {activeMenu} from './modules/active-menu'
+
+
+import {MDCRipple} from '@material/ripple';
+const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
+
+//import {MDCTabBar} from '@material/tab-bar';
+//const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
+
+
+
+//import {MDCFormField} from '@material/form-field';
+
+//const formField = new MDCFormField(document.querySelector('.mdc-form-field'));
+//formField.input = checkbox;
+
+import {MDCTextField} from '@material/textfield';
+
+const textField = new MDCTextField(document.querySelector('.js-registro-nombre'));
+const textField2 = new MDCTextField(document.querySelector('.js-form-correo'));
+const textField3 = new MDCTextField(document.querySelector('.js-form-pass'));
+const textField4 = new MDCTextField(document.querySelector('.js-form-confirm-pass'));
+const textField5 = new MDCTextField(document.querySelector('.js-form-local'));
+const textField6 = new MDCTextField(document.querySelector('.js-form-mesas'));
+const textField7 = new MDCTextField(document.querySelector('.js-contact-nombre'));
+const textField8 = new MDCTextField(document.querySelector('.js-contact-telefono'));
+const textField9 = new MDCTextField(document.querySelector('.js-contact-correo'));
+const textField10 = new MDCTextField(document.querySelector('.js-contact-mensaje'));
+
+
+import {MDCTextFieldHelperText} from '@material/textfield/helper-text';
+
+const helperText = new MDCTextFieldHelperText(document.querySelector('.mdc-text-field-helper-text'));
+
+import {MDCCheckbox} from '@material/checkbox';
+
+const checkbox = new MDCCheckbox(document.querySelector('.mdc-checkbox'));
---
+++ src/js/modules/active-menu.js
@@ -0,0 +1,45 @@
+const menu = document.getElementById('main-menu')
+
+export const activeMenu = () => {
+  if (menu) {
+      const links = Array.from(menu.querySelectorAll('a'))
+      links.map(link => {
+          if (link.href === location.href || link.href === location.href.slice(0,-1)) link.classList.add('active')
+      })
+  }
+}
+
+const hamburgerMenuBox = document.getElementById('js-hamburger-box');
+const hamburgerMenu = document.createElement('div');
+hamburgerMenu.classList.add('mdc-button');
+
+const iconMenu = new Image();
+iconMenu.src = '/assets/img/ic_menu_white_36pt.png';
+
+hamburgerMenu.innerHTML = `
+<img src="${iconMenu.src}" alt="Menu"/>
+`;
+
+if(window.innerWidth < 950){
+    hamburgerMenuBox.appendChild(hamburgerMenu)
+}
+const injectMenu = innerWidth => {
+    if(innerWidth < 950){
+        hamburgerMenuBox.appendChild(hamburgerMenu)
+    }else {
+        hamburgerMenuBox.innerHTML = ``
+    }
+}
+
+const callMenu = () => {
+    menu.classList.toggle('main-menu__active')
+}
+
+if(hamburgerMenuBox){
+    window.addEventListener('resize', e => {
+        injectMenu(window.innerWidth)
+    })
+    hamburgerMenuBox.addEventListener('click', (e) =>{
+        callMenu()
+    })
+}
